c06e13a665401d8080a5aade4a7cf4758c05d1ef
Tessa Kelly <tessakkelly@gmail.com>
2017-03-17 11:05:33 -0700

    Adds base for html a11y

---
+++ src/Html/Attributes/A11y.elm
@@ -0,0 +1,18 @@
+module Html.Attributes.A11y exposing (view)
+
+{-|
+@docs view
+-}
+
+import Html exposing (..)
+import Html.Attributes
+
+
+{-| view
+-}
+view : Html msg
+view =
+    div
+        []
+        [ text "Hello, world!"
+        ]
