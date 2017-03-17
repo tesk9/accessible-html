8408e597b8aa4938a599cff65e520d2129ecdc13
Tessa Kelly <tessakkelly@gmail.com>
2017-03-17 13:25:41 -0700

    :art: extract all-input base tests

--- tests/Html/A11ySpec.elm
+++ tests/Html/A11ySpec.elm
@@ -32,45 +32,19 @@ inputTests toView =
                     , attributes = []
                     }
             in
-                [ test "has label with the given label text" <|
-                    \() ->
-                        queryView mockInputModel
-                            |> Query.find [ Selector.tag "label" ]
-                            |> Query.has [ Selector.text "Name" ]
-                , test "has input with the appropriate value" <|
-                    \() ->
-                        queryView mockInputModel
-                            |> Query.find [ Selector.tag "input" ]
-                            |> Query.has [ Selector.attribute "value" "Tessa" ]
-                , test "is an input of the appropriate type" <|
-                    \() ->
-                        queryView mockInputModel
-                            |> Query.find [ Selector.tag "input" ]
-                            |> Query.has [ Selector.attribute "type" "text" ]
+                [ baseInputTests (queryView mockInputModel)
+                    { label = "Name", value = "Tessa", type_ = "text" }
                 ]
         , describe "radioInput" <|
             let
                 mockInputModel =
-                    { label = text "Name"
+                    { label = text "Radio input"
                     , typeAndValue = radioInput "radio-group-name" "8" True
                     , attributes = []
                     }
             in
-                [ test "has label with the given label text" <|
-                    \() ->
-                        queryView mockInputModel
-                            |> Query.find [ Selector.tag "label" ]
-                            |> Query.has [ Selector.text "Name" ]
-                , test "has input with the appropriate value" <|
-                    \() ->
-                        queryView mockInputModel
-                            |> Query.find [ Selector.tag "input" ]
-                            |> Query.has [ Selector.attribute "value" "8" ]
-                , test "is an input of the appropriate type" <|
-                    \() ->
-                        queryView mockInputModel
-                            |> Query.find [ Selector.tag "input" ]
-                            |> Query.has [ Selector.attribute "type" "radio" ]
+                [ baseInputTests (queryView mockInputModel)
+                    { label = "Radio input", value = "8", type_ = "radio" }
                 , test "is checked" <|
                     \() ->
                         queryView mockInputModel
@@ -80,26 +54,13 @@ inputTests toView =
         , describe "checkboxInput" <|
             let
                 mockInputModel maybeSelected =
-                    { label = text "Name"
+                    { label = text "To check or not to check?"
                     , typeAndValue = checkboxInput "some sick value" maybeSelected
                     , attributes = []
                     }
             in
-                [ test "has label with the given label text" <|
-                    \() ->
-                        queryView (mockInputModel (Just True))
-                            |> Query.find [ Selector.tag "label" ]
-                            |> Query.has [ Selector.text "Name" ]
-                , test "has input with the appropriate value" <|
-                    \() ->
-                        queryView (mockInputModel (Just True))
-                            |> Query.find [ Selector.tag "input" ]
-                            |> Query.has [ Selector.attribute "value" "some sick value" ]
-                , test "is an input of the appropriate type" <|
-                    \() ->
-                        queryView (mockInputModel (Just True))
-                            |> Query.find [ Selector.tag "input" ]
-                            |> Query.has [ Selector.attribute "type" "checkbox" ]
+                [ baseInputTests (queryView <| mockInputModel (Just True))
+                    { label = "To check or not to check?", value = "some sick value", type_ = "checkbox" }
                 , test "is checked" <|
                     \() ->
                         queryView (mockInputModel (Just True))
@@ -116,4 +77,25 @@ inputTests toView =
                             |> Query.find [ Selector.tag "input" ]
                             |> Query.has [ Selector.boolAttribute "indeterminate" True ]
                 ]
+        ]
+
+
+baseInputTests : Query.Single -> { label : String, value : String, type_ : String } -> Test
+baseInputTests queryView { label, value, type_ } =
+    describe "Basic input tests"
+        [ test "has label with the given label text" <|
+            \() ->
+                queryView
+                    |> Query.find [ Selector.tag "label" ]
+                    |> Query.has [ Selector.text label ]
+        , test "has input with the appropriate value" <|
+            \() ->
+                queryView
+                    |> Query.find [ Selector.tag "input" ]
+                    |> Query.has [ Selector.attribute "value" value ]
+        , test "is an input of the appropriate type" <|
+            \() ->
+                queryView
+                    |> Query.find [ Selector.tag "input" ]
+                    |> Query.has [ Selector.attribute "type" type_ ]
         ]
