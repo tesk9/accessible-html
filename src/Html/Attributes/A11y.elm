dd1056264d11c10464a5603455aae5ba383f338e
Tessa Kelly <tessakkelly@gmail.com>
2017-03-18 20:00:13 -0700

    Switch to defined list of aria roles

--- src/Html/Attributes/A11y.elm
+++ src/Html/Attributes/A11y.elm
@@ -1,11 +1,18 @@
-module Html.Attributes.A11y exposing (invisible, role)
+module Html.Attributes.A11y exposing (invisible, Role(..), role)
 
 {-|
-@docs invisible
+#### Hiding content
 
 For more information on hiding/semi-hiding elements, please see [the a11y project.](http://a11yproject.com/posts/how-to-hide-content/)
 
-@docs role
+@docs invisible
+
+#### Roles
+
+Learn more about roles on the [w3 website](https://www.w3.org/TR/wai-aria/roles).
+
+@docs Role, role
+
 -}
 
 import Css
@@ -14,6 +21,7 @@ import Html.Attributes exposing (..)
 
 
 {-| Makes content invisible without making it inaccessible.
+
     label [ invisible ] [ text "Screen readers can still read me!" ]
 -}
 invisible : Html.Attribute msg
@@ -31,9 +39,275 @@ invisible =
             ]
 
 
-{-| Set the role of a given element
-    div [ role "tablist" ] [ tab1, tab2 ]
+{-| Set the role of a given element.
+
+    div [ role Tablist ] [ tab1, tab2 ]
 -}
-role : String -> Html.Attribute msg
-role =
+role : Role -> Html.Attribute msg
+role role_ =
+    customRole <| roleToString role_
+
+
+{-| Set the role of a given element to be a given string.
+
+    div [ customRole "tablist" ] [ tab1, tab2 ]
+-}
+customRole : String -> Html.Attribute msg
+customRole =
     attribute "role"
+
+
+{-|
+The roles defined here match [w3 website](https://www.w3.org/TR/wai-aria/roles).
+
+To be used with `role`.
+-}
+type Role
+    = Alert
+    | Alertdialog
+    | Application
+    | Article
+    | Banner
+    | Button
+    | Checkbox
+    | Columnheader
+    | Combobox
+    | Complementary
+    | Contentinfo
+    | Definition
+    | Dialog
+    | Directory
+    | Document
+    | Form
+    | Grid
+    | Gridcell
+    | Group
+    | Heading
+    | Img
+    | Link
+    | List
+    | Listbox
+    | Listitem
+    | Log
+    | Main
+    | Marquee
+    | Math
+    | Menu
+    | Menubar
+    | Menuitem
+    | Menuitemcheckbox
+    | Menuitemradio
+    | Navigation
+    | Note
+    | Option
+    | Presentation
+    | Progressbar
+    | Radio
+    | Radiogroup
+    | Region
+    | Row
+    | Rowgroup
+    | Rowheader
+    | Scrollbar
+    | Search
+    | Separator
+    | Slider
+    | Spinbutton
+    | Status
+    | Tab
+    | Tablist
+    | Tabpanel
+    | Textbox
+    | Timer
+    | Toolbar
+    | Tooltip
+    | Tree
+    | Treegrid
+    | Treeitem
+
+
+roleToString : Role -> String
+roleToString role =
+    case role of
+        Alert ->
+            "alert"
+
+        Alertdialog ->
+            "alertdialog"
+
+        Application ->
+            "application"
+
+        Article ->
+            "article"
+
+        Banner ->
+            "banner"
+
+        Button ->
+            "button"
+
+        Checkbox ->
+            "checkbox"
+
+        Columnheader ->
+            "columnheader"
+
+        Combobox ->
+            "combobox"
+
+        Complementary ->
+            "complementary"
+
+        Contentinfo ->
+            "contentinfo"
+
+        Definition ->
+            "definition"
+
+        Dialog ->
+            "dialog"
+
+        Directory ->
+            "directory"
+
+        Document ->
+            "document"
+
+        Form ->
+            "form"
+
+        Grid ->
+            "grid"
+
+        Gridcell ->
+            "gridcell"
+
+        Group ->
+            "group"
+
+        Heading ->
+            "heading"
+
+        Img ->
+            "img"
+
+        Link ->
+            "link"
+
+        List ->
+            "list"
+
+        Listbox ->
+            "listbox"
+
+        Listitem ->
+            "listitem"
+
+        Log ->
+            "log"
+
+        Main ->
+            "main"
+
+        Marquee ->
+            "marquee"
+
+        Math ->
+            "math"
+
+        Menu ->
+            "menu"
+
+        Menubar ->
+            "menubar"
+
+        Menuitem ->
+            "menuitem"
+
+        Menuitemcheckbox ->
+            "menuitemcheckbox"
+
+        Menuitemradio ->
+            "menuitemradio"
+
+        Navigation ->
+            "navigation"
+
+        Note ->
+            "note"
+
+        Option ->
+            "option"
+
+        Presentation ->
+            "presentation"
+
+        Progressbar ->
+            "progressbar"
+
+        Radio ->
+            "radio"
+
+        Radiogroup ->
+            "radiogroup"
+
+        Region ->
+            "region"
+
+        Row ->
+            "row"
+
+        Rowgroup ->
+            "rowgroup"
+
+        Rowheader ->
+            "rowheader"
+
+        Scrollbar ->
+            "scrollbar"
+
+        Search ->
+            "search"
+
+        Separator ->
+            "separator"
+
+        Slider ->
+            "slider"
+
+        Spinbutton ->
+            "spinbutton"
+
+        Status ->
+            "status"
+
+        Tab ->
+            "tab"
+
+        Tablist ->
+            "tablist"
+
+        Tabpanel ->
+            "tabpanel"
+
+        Textbox ->
+            "textbox"
+
+        Timer ->
+            "timer"
+
+        Toolbar ->
+            "toolbar"
+
+        Tooltip ->
+            "tooltip"
+
+        Tree ->
+            "tree"
+
+        Treegrid ->
+            "treegrid"
+
+        Treeitem ->
+            "treeitem"
--- tests/Html/Attributes/A11ySpec.elm
+++ tests/Html/Attributes/A11ySpec.elm
@@ -10,13 +10,13 @@ import Test.Html.Selector as Selector
 spec : Test
 spec =
     describe "Html.Attributes.A11ySpec"
-        [ describe "role" [ addsRole "tablist" ] ]
+        [ describe "role" [ addsRole Tablist "tablist" ] ]
 
 
-addsRole : String -> Test
-addsRole role_ =
-    test ("sets the role attribute: " ++ role_) <|
+addsRole : Role -> String -> Test
+addsRole role_ expected =
+    test ("sets the role attribute: " ++ toString role_) <|
         \() ->
             div [] [ div [ role role_ ] [] ]
                 |> Query.fromHtml
-                |> Query.has [ Selector.attribute "role" role_ ]
+                |> Query.has [ Selector.attribute "role" expected ]
