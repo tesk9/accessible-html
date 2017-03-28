ccb603b68163f5588a3500d0bb8d85dc36d937dd
Tessa Kelly <tessakkelly@gmail.com>
2017-03-27 19:49:39 -0700

    Expand tests to be for each role

--- tests/Html/Attributes/A11ySpec.elm
+++ tests/Html/Attributes/A11ySpec.elm
@@ -10,7 +10,9 @@ import Test.Html.Selector as Selector
 spec : Test
 spec =
     describe "Html.Attributes.A11ySpec"
-        [ describe "role" [ addsRole Tablist "tablist" ] ]
+        [ describe "role" <|
+            List.map (uncurry addsRole) allRoles
+        ]
 
 
 addsRole : Role -> String -> Test
@@ -20,3 +22,69 @@ addsRole role_ expected =
             div [] [ div [ role role_ ] [] ]
                 |> Query.fromHtml
                 |> Query.has [ Selector.attribute "role" expected ]
+
+
+allRoles : List ( Role, String )
+allRoles =
+    [ ( Alert, "alert" )
+    , ( Alertdialog, "alertdialog" )
+    , ( Application, "application" )
+    , ( Article, "article" )
+    , ( Banner, "banner" )
+    , ( Button, "button" )
+    , ( Checkbox, "checkbox" )
+    , ( Columnheader, "columnheader" )
+    , ( Combobox, "combobox" )
+    , ( Complementary, "complementary" )
+    , ( Contentinfo, "contentinfo" )
+    , ( Definition, "definition" )
+    , ( Dialog, "dialog" )
+    , ( Directory, "directory" )
+    , ( Document, "document" )
+    , ( Form, "form" )
+    , ( Grid, "grid" )
+    , ( Gridcell, "gridcell" )
+    , ( Group, "group" )
+    , ( Heading, "heading" )
+    , ( Img, "img" )
+    , ( Link, "link" )
+    , ( List, "list" )
+    , ( Listbox, "listbox" )
+    , ( Listitem, "listitem" )
+    , ( Log, "log" )
+    , ( Main, "main" )
+    , ( Marquee, "marquee" )
+    , ( Math, "math" )
+    , ( Menu, "menu" )
+    , ( Menubar, "menubar" )
+    , ( Menuitem, "menuitem" )
+    , ( Menuitemcheckbox, "menuitemcheckbox" )
+    , ( Menuitemradio, "menuitemradio" )
+    , ( Navigation, "navigation" )
+    , ( Note, "note" )
+    , ( Option, "option" )
+    , ( Presentation, "presentation" )
+    , ( Progressbar, "progressbar" )
+    , ( Radio, "radio" )
+    , ( Radiogroup, "radiogroup" )
+    , ( Region, "region" )
+    , ( Row, "row" )
+    , ( Rowgroup, "rowgroup" )
+    , ( Rowheader, "rowheader" )
+    , ( Scrollbar, "scrollbar" )
+    , ( Search, "search" )
+    , ( Separator, "separator" )
+    , ( Slider, "slider" )
+    , ( Spinbutton, "spinbutton" )
+    , ( Status, "status" )
+    , ( Tab, "tab" )
+    , ( Tablist, "tablist" )
+    , ( Tabpanel, "tabpanel" )
+    , ( Textbox, "textbox" )
+    , ( Timer, "timer" )
+    , ( Toolbar, "toolbar" )
+    , ( Tooltip, "tooltip" )
+    , ( Tree, "tree" )
+    , ( Treegrid, "treegrid" )
+    , ( Treeitem, "treeitem" )
+    ]
