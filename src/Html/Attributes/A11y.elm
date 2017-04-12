272c1844bcc763a86a04557c02e2eb06fddad658
Tessa Kelly <tessakkelly@gmail.com>
2017-04-12 08:39:01 -0700

    Improve doc comments for element attribtues

--- src/Html/Attributes/A11y.elm
+++ src/Html/Attributes/A11y.elm
@@ -268,336 +268,336 @@ heading =
     Heading.heading
 
 
-{-| Add `alert` to the attributes of an element.
+{-| Add `role="alert"` to the attributes of an element.
 -}
 alert : Html.Attribute msg
 alert =
     Element.alert
 
 
-{-| Add `alertdialog` to the attributes of an element.
+{-| Add `role="alertdialog"` to the attributes of an element.
 -}
 alertdialog : Html.Attribute msg
 alertdialog =
     Element.alertdialog
 
 
-{-| Add `article` to the attributes of an element.
+{-| Add `role="article"` to the attributes of an element.
 -}
 article : Html.Attribute msg
 article =
     Element.article
 
 
-{-| Add `button` to the attributes of an element.
+{-| Add `role="button"` to the attributes of an element.
 -}
 button : Html.Attribute msg
 button =
     Element.button
 
 
-{-| Add `checkbox` to the attributes of an element.
+{-| Add `role="checkbox"` to the attributes of an element.
 -}
 checkbox : Html.Attribute msg
 checkbox =
     Element.checkbox
 
 
-{-| Add `columnheader` to the attributes of an element.
+{-| Add `role="columnheader"` to the attributes of an element.
 -}
 columnheader : Html.Attribute msg
 columnheader =
     Element.columnheader
 
 
-{-| Add `combobox` to the attributes of an element.
+{-| Add `role="combobox"` to the attributes of an element.
 -}
 combobox : Html.Attribute msg
 combobox =
     Element.combobox
 
 
-{-| Add `definition` to the attributes of an element.
+{-| Add `role="definition"` to the attributes of an element.
 -}
 definition : Html.Attribute msg
 definition =
     Element.definition
 
 
-{-| Add `dialog` to the attributes of an element.
+{-| Add `role="dialog"` to the attributes of an element.
 -}
 dialog : Html.Attribute msg
 dialog =
     Element.dialog
 
 
-{-| Add `directory` to the attributes of an element.
+{-| Add `role="directory"` to the attributes of an element.
 -}
 directory : Html.Attribute msg
 directory =
     Element.directory
 
 
-{-| Add `document` to the attributes of an element.
+{-| Add `role="document"` to the attributes of an element.
 -}
 document : Html.Attribute msg
 document =
     Element.document
 
 
-{-| Add `grid` to the attributes of an element.
+{-| Add `role="grid"` to the attributes of an element.
 -}
 grid : Html.Attribute msg
 grid =
     Element.grid
 
 
-{-| Add `gridcell` to the attributes of an element.
+{-| Add `role="gridcell"` to the attributes of an element.
 -}
 gridcell : Html.Attribute msg
 gridcell =
     Element.gridcell
 
 
-{-| Add `img` to the attributes of an element.
+{-| Add `role="img"` to the attributes of an element.
 -}
 img : Html.Attribute msg
 img =
     Element.img
 
 
-{-| Add `link` to the attributes of an element.
+{-| Add `role="link"` to the attributes of an element.
 -}
 link : Html.Attribute msg
 link =
     Element.link
 
 
-{-| Add `list` to the attributes of an element.
+{-| Add `role="list"` to the attributes of an element.
 -}
 list : Html.Attribute msg
 list =
     Element.list
 
 
-{-| Add `listbox` to the attributes of an element.
+{-| Add `role="listbox"` to the attributes of an element.
 -}
 listbox : Html.Attribute msg
 listbox =
     Element.listbox
 
 
-{-| Add `listitem` to the attributes of an element.
+{-| Add `role="listitem"` to the attributes of an element.
 -}
 listitem : Html.Attribute msg
 listitem =
     Element.listitem
 
 
-{-| Add `log` to the attributes of an element.
+{-| Add `role="log"` to the attributes of an element.
 -}
 log : Html.Attribute msg
 log =
     Element.log
 
 
-{-| Add `marquee` to the attributes of an element.
+{-| Add `role="marquee"` to the attributes of an element.
 -}
 marquee : Html.Attribute msg
 marquee =
     Element.marquee
 
 
-{-| Add `math` to the attributes of an element.
+{-| Add `role="math"` to the attributes of an element.
 -}
 math : Html.Attribute msg
 math =
     Element.math
 
 
-{-| Add `menu` to the attributes of an element.
+{-| Add `role="menu"` to the attributes of an element.
 -}
 menu : Html.Attribute msg
 menu =
     Element.menu
 
 
-{-| Add `menubar` to the attributes of an element.
+{-| Add `role="menubar"` to the attributes of an element.
 -}
 menubar : Html.Attribute msg
 menubar =
     Element.menubar
 
 
-{-| Add `menuitem` to the attributes of an element.
+{-| Add `role="menuitem"` to the attributes of an element.
 -}
 menuitem : Html.Attribute msg
 menuitem =
     Element.menuitem
 
 
-{-| Add `menuitemcheckbox` to the attributes of an element.
+{-| Add `role="menuitemcheckbox"` to the attributes of an element.
 -}
 menuitemcheckbox : Html.Attribute msg
 menuitemcheckbox =
     Element.menuitemcheckbox
 
 
-{-| Add `menuitemradio` to the attributes of an element.
+{-| Add `role="menuitemradio"` to the attributes of an element.
 -}
 menuitemradio : Html.Attribute msg
 menuitemradio =
     Element.menuitemradio
 
 
-{-| Add `note` to the attributes of an element.
+{-| Add `role="note"` to the attributes of an element.
 -}
 note : Html.Attribute msg
 note =
     Element.note
 
 
-{-| Add `option` to the attributes of an element.
+{-| Add `role="option"` to the attributes of an element.
 -}
 option : Html.Attribute msg
 option =
     Element.option
 
 
-{-| Add `progressbar` to the attributes of an element.
+{-| Add `role="progressbar"` to the attributes of an element.
 -}
 progressbar : Html.Attribute msg
 progressbar =
     Element.progressbar
 
 
-{-| Add `radio` to the attributes of an element.
+{-| Add `role="radio"` to the attributes of an element.
 -}
 radio : Html.Attribute msg
 radio =
     Element.radio
 
 
-{-| Add `row` to the attributes of an element.
+{-| Add `role="row"` to the attributes of an element.
 -}
 row : Html.Attribute msg
 row =
     Element.row
 
 
-{-| Add `rowgroup` to the attributes of an element.
+{-| Add `role="rowgroup"` to the attributes of an element.
 -}
 rowgroup : Html.Attribute msg
 rowgroup =
     Element.rowgroup
 
 
-{-| Add `rowheader` to the attributes of an element.
+{-| Add `role="rowheader"` to the attributes of an element.
 -}
 rowheader : Html.Attribute msg
 rowheader =
     Element.rowheader
 
 
-{-| Add `scrollbar` to the attributes of an element.
+{-| Add `role="scrollbar"` to the attributes of an element.
 -}
 scrollbar : Html.Attribute msg
 scrollbar =
     Element.scrollbar
 
 
-{-| Add `separator` to the attributes of an element.
+{-| Add `role="separator"` to the attributes of an element.
 -}
 separator : Html.Attribute msg
 separator =
     Element.separator
 
 
-{-| Add `slider` to the attributes of an element.
+{-| Add `role="slider"` to the attributes of an element.
 -}
 slider : Html.Attribute msg
 slider =
     Element.slider
 
 
-{-| Add `spinbutton` to the attributes of an element.
+{-| Add `role="spinbutton"` to the attributes of an element.
 -}
 spinbutton : Html.Attribute msg
 spinbutton =
     Element.spinbutton
 
 
-{-| Add `status` to the attributes of an element.
+{-| Add `role="status"` to the attributes of an element.
 -}
 status : Html.Attribute msg
 status =
     Element.status
 
 
-{-| Add `tab` to the attributes of an element.
+{-| Add `role="tab"` to the attributes of an element.
 -}
 tab : Html.Attribute msg
 tab =
     Element.tab
 
 
-{-| Add `tablist` to the attributes of an element.
+{-| Add `role="tablist"` to the attributes of an element.
 -}
 tablist : Html.Attribute msg
 tablist =
     Element.tablist
 
 
-{-| Add `tabpanel` to the attributes of an element.
+{-| Add `role="tabpanel"` to the attributes of an element.
 -}
 tabpanel : Html.Attribute msg
 tabpanel =
     Element.tabpanel
 
 
-{-| Add `textbox` to the attributes of an element.
+{-| Add `role="textbox"` to the attributes of an element.
 -}
 textbox : Html.Attribute msg
 textbox =
     Element.textbox
 
 
-{-| Add `timer` to the attributes of an element.
+{-| Add `role="timer"` to the attributes of an element.
 -}
 timer : Html.Attribute msg
 timer =
     Element.timer
 
 
-{-| Add `toolbar` to the attributes of an element.
+{-| Add `role="toolbar"` to the attributes of an element.
 -}
 toolbar : Html.Attribute msg
 toolbar =
     Element.toolbar
 
 
-{-| Add `tooltip` to the attributes of an element.
+{-| Add `role="tooltip"` to the attributes of an element.
 -}
 tooltip : Html.Attribute msg
 tooltip =
     Element.tooltip
 
 
-{-| Add `tree` to the attributes of an element.
+{-| Add `role="tree"` to the attributes of an element.
 -}
 tree : Html.Attribute msg
 tree =
     Element.tree
 
 
-{-| Add `treegrid` to the attributes of an element.
+{-| Add `role="treegrid"` to the attributes of an element.
 -}
 treegrid : Html.Attribute msg
 treegrid =
     Element.treegrid
 
 
-{-| Add `treeitem` to the attributes of an element.
+{-| Add `role="treeitem"` to the attributes of an element.
 -}
 treeitem : Html.Attribute msg
 treeitem =
