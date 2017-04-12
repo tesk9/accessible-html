ff11221d62b22e2b42107e1ea732bd50a362ef43
Tessa Kelly <tessakkelly@gmail.com>
2017-04-11 22:36:30 -0700

    Add in Aria

---
+++ src/Aria.elm
@@ -0,0 +1,48 @@
+module Aria
+    exposing
+        ( controls
+        , labelledBy
+        , selected
+        , hidden
+        , longDescription
+        , indeterminate
+        )
+
+import Html
+import Html.Attributes exposing (..)
+import Json.Encode
+
+
+controls : String -> Html.Attribute msg
+controls =
+    attribute "aria-controls"
+
+
+labelledBy : String -> Html.Attribute msg
+labelledBy =
+    attribute "aria-labelledby"
+
+
+selected : Bool -> Html.Attribute msg
+selected =
+    attribute "aria-selected" << String.toLower << toString
+
+
+hidden : Bool -> Html.Attribute msg
+hidden =
+    attribute "aria-hidden" << String.toLower << toString
+
+
+longDescription : String -> Html.Attribute msg
+longDescription =
+    attribute "longDesc"
+
+
+indeterminate : Html.Attribute msg
+indeterminate =
+    property "indeterminate" (Json.Encode.bool True)
+
+
+level : Int -> Html.Attribute msg
+level =
+    (attribute "aria-level" << toString)
