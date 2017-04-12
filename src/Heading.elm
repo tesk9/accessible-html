09243be5dd8e315fdda586b4ed3eb3d94e5ffc05
Tessa Kelly <tessakkelly@gmail.com>
2017-04-11 22:24:23 -0700

    Adds Heading module

--- src/Heading.elm
+++ src/Heading.elm
@@ -1,13 +1,18 @@
 module Heading exposing (heading)
 
-{-| Prefer using `h1`, `h2`, `h3`, `h4`, `h5`, and `h6`.
-Really this module should only be necessary if you need an `h7`-type heading.
+{-|
+@docs heading
 -}
 
 import Html
 import Role exposing (..)
 
 
+{-| Prefer using `h1`, `h2`, `h3`, `h4`, `h5`, and `h6`.
+Really this attribute should only be necessary if you need an `h7`-type heading.
+
+    div [ heading, level 7 ] []
+-}
 heading : Html.Attribute msg
 heading =
     role Heading
