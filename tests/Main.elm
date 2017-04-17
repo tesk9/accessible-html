21e73ed1d2c0e4b802208f20f9b60a37a4c341b8
Tessa Kelly <tessakkelly@gmail.com>
2017-04-16 22:40:54 -0700

    Copy input tests into a separate set of tests

--- tests/Main.elm
+++ tests/Main.elm
@@ -2,9 +2,10 @@ port module Main exposing (..)
 
 import Html.A11ySpec
 import Html.Attributes.A11ySpec
+import Json.Encode exposing (Value)
+import Tags.InputsSpec
 import Test
 import Test.Runner.Node exposing (run, TestProgram)
-import Json.Encode exposing (Value)
 
 
 main : TestProgram
@@ -13,6 +14,7 @@ main =
         Test.concat
             [ Html.A11ySpec.spec
             , Html.Attributes.A11ySpec.spec
+            , Tags.InputsSpec.spec
             ]
 
 
