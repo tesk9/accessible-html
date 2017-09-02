8722f639036ff295c6e052c26cf28418d4252bc9
Tessa Kelly <tessakkelly@gmail.com>
2017-09-02 16:45:43 -0700

    Extract common functionality

--- tests/Accessibility/WidgetSpec.elm
+++ tests/Accessibility/WidgetSpec.elm
@@ -4,6 +4,7 @@ import Accessibility.Widget exposing (..)
 import Html
 import Html.Attributes
 import Json.Encode
+import SpecHelpers exposing (..)
 import Test exposing (..)
 import Test.Html.Query as Query
 import Test.Html.Selector as Selector
@@ -46,104 +47,3 @@ spec =
         , addsAriaNumAttribute valueNow "valuenow"
         , addsAriaNumAttribute level "level"
         ]
-
-
-addsAttribute : Html.Attribute msg -> ( String, String ) -> Test
-addsAttribute setter ( attribute, content ) =
-    test ("sets the " ++ toString setter ++ " attribute") <|
-        \() ->
-            Html.div [] [ Html.div [ setter ] [] ]
-                |> Query.fromHtml
-                |> Query.has
-                    [ Selector.attribute <|
-                        Html.Attributes.property attribute (Json.Encode.string content)
-                    ]
-
-
-addsStringAttribute : (String -> Html.Attribute msg) -> ( String, String ) -> Test
-addsStringAttribute setter ( attribute, content ) =
-    test ("sets the " ++ toString setter ++ " attribute") <|
-        \() ->
-            Html.div [] [ Html.div [ setter content ] [] ]
-                |> Query.fromHtml
-                |> Query.has
-                    [ Selector.attribute <|
-                        Html.Attributes.property attribute (Json.Encode.string content)
-                    ]
-
-
-addsBoolAttribute : (Bool -> Html.Attribute msg) -> String -> Test
-addsBoolAttribute setter attribute =
-    let
-        adds bool =
-            \() ->
-                Html.div [] [ Html.div [ setter bool ] [] ]
-                    |> Query.fromHtml
-                    |> Query.has
-                        [ Selector.attribute <|
-                            Html.Attributes.property attribute
-                                ((Json.Encode.string << String.toLower << toString) bool)
-                        ]
-    in
-    describe ("sets the " ++ toString setter ++ " attribute")
-        [ test "True" <| adds True
-        , test "False" <| adds False
-        ]
-
-
-addsAriaAttribute : Html.Attribute msg -> ( String, String ) -> Test
-addsAriaAttribute setter ( attribute, content ) =
-    addsAttribute setter ( "aria-" ++ attribute, content )
-
-
-addsAriaStringAttribute : (String -> Html.Attribute msg) -> ( String, String ) -> Test
-addsAriaStringAttribute setter ( attribute, content ) =
-    addsStringAttribute setter ( "aria-" ++ attribute, content )
-
-
-addsAriaBoolAttribute : (Bool -> Html.Attribute msg) -> String -> Test
-addsAriaBoolAttribute setter attribute =
-    addsBoolAttribute setter ("aria-" ++ attribute)
-
-
-addsAriaListStringAttribute : (List String -> Html.Attribute msg) -> ( String, String ) -> Test
-addsAriaListStringAttribute setter ( attribute, content ) =
-    addsAttribute (setter [ content ]) ( "aria-" ++ attribute, content )
-
-
-addsAriaTristateAttribute : (Maybe Bool -> Html.Attribute msg) -> String -> Test
-addsAriaTristateAttribute setter attribute =
-    let
-        adds state desiredState =
-            \() ->
-                Html.div [] [ Html.div [ setter state ] [] ]
-                    |> Query.fromHtml
-                    |> Query.has
-                        [ Selector.attribute <|
-                            Html.Attributes.property ("aria-" ++ attribute)
-                                (Json.Encode.string desiredState)
-                        ]
-    in
-    describe ("sets the " ++ toString setter ++ " attribute")
-        [ test "True" <| adds (Just True) "true"
-        , test "False" <| adds (Just False) "false"
-        , test "Mixed" <| adds Nothing "mixed"
-        ]
-
-
-addsAriaNumAttribute : (number -> Html.Attribute msg) -> String -> Test
-addsAriaNumAttribute setter attribute =
-    --TODO: Fuzz?
-    let
-        adds state =
-            \() ->
-                Html.div [] [ Html.div [ setter state ] [] ]
-                    |> Query.fromHtml
-                    |> Query.has
-                        [ Selector.attribute <|
-                            Html.Attributes.property ("aria-" ++ attribute)
-                                ((Json.Encode.string << toString) state)
-                        ]
-    in
-    describe ("sets the " ++ toString setter ++ " attribute")
-        [ test "True" <| adds 8 ]
---
+++ tests/SpecHelpers.elm
@@ -0,0 +1,94 @@
+module SpecHelpers exposing (..)
+
+import Expect
+import Html
+import Html.Attributes
+import Json.Encode
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector as Selector
+
+
+addsAttribute : Html.Attribute msg -> ( String, String ) -> Test
+addsAttribute setter ( attribute, content ) =
+    test ("sets the " ++ toString setter ++ " attribute") <|
+        expectAttribute ( \() -> setter, () ) ( attribute, content )
+
+
+addsStringAttribute : (String -> Html.Attribute msg) -> ( String, String ) -> Test
+addsStringAttribute setter ( attribute, content ) =
+    test ("sets the " ++ toString setter ++ " attribute") <|
+        expectAttribute ( setter, content ) ( attribute, content )
+
+
+addsBoolAttribute : (Bool -> Html.Attribute msg) -> String -> Test
+addsBoolAttribute setter attribute =
+    describe ("sets the " ++ toString setter ++ " attribute")
+        [ test "True" <| expectAttribute ( setter, True ) ( attribute, "true" )
+        , test "False" <| expectAttribute ( setter, False ) ( attribute, "false" )
+        ]
+
+
+addsAriaAttribute : Html.Attribute msg -> ( String, String ) -> Test
+addsAriaAttribute setter ( attribute, content ) =
+    addsAttribute setter ( "aria-" ++ attribute, content )
+
+
+addsAriaStringAttribute : (String -> Html.Attribute msg) -> ( String, String ) -> Test
+addsAriaStringAttribute setter ( attribute, content ) =
+    addsStringAttribute setter ( "aria-" ++ attribute, content )
+
+
+addsAriaBoolAttribute : (Bool -> Html.Attribute msg) -> String -> Test
+addsAriaBoolAttribute setter attribute =
+    addsBoolAttribute setter ("aria-" ++ attribute)
+
+
+addsAriaListStringAttribute : (List String -> Html.Attribute msg) -> ( String, String ) -> Test
+addsAriaListStringAttribute setter ( attribute, content ) =
+    addsAttribute (setter [ content ]) ( "aria-" ++ attribute, content )
+
+
+addsAriaTristateAttribute : (Maybe Bool -> Html.Attribute msg) -> String -> Test
+addsAriaTristateAttribute setter attribute =
+    describe ("sets the " ++ toString setter ++ " attribute")
+        [ test "True" <| expectAria ( setter, Just True ) ( attribute, "true" )
+        , test "False" <| expectAria ( setter, Just False ) ( attribute, "false" )
+        , test "Mixed" <| expectAria ( setter, Nothing ) ( attribute, "mixed" )
+        ]
+
+
+addsAriaNumAttribute : (number -> Html.Attribute msg) -> String -> Test
+addsAriaNumAttribute setter attribute =
+    describe ("sets the " ++ toString setter ++ " attribute")
+        [ test "True" <| expectAria ( setter, 8 ) ( attribute, "8" ) ]
+
+
+expectAria :
+    ( a -> Html.Attribute msg, a )
+    -> ( String, b )
+    -> ()
+    -> Expect.Expectation
+expectAria ( setter, state ) ( attribute, attrState ) =
+    expectAttribute ( setter, state ) ( "aria-" ++ attribute, attrState )
+
+
+expectAttribute :
+    ( a -> Html.Attribute msg, a )
+    -> ( String, b )
+    -> ()
+    -> Expect.Expectation
+expectAttribute ( setter, state ) ( attribute, attrState ) =
+    \() ->
+        Html.div [] [ Html.div [ setter state ] [] ]
+            |> Query.fromHtml
+            |> hasAttribute attribute attrState
+
+
+hasAttribute : String -> a -> Query.Single msg -> Expect.Expectation
+hasAttribute attribute state =
+    Query.has
+        [ Selector.attribute <|
+            Html.Attributes.property attribute
+                ((Json.Encode.string << toString) state)
+        ]
