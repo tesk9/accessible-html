f1c516659fa3e637a6a8058b54969cfc19441489
Tessa Kelly <tessakkelly@gmail.com>
2018-10-20 16:03:02 -0700

    Add compiled index html

--- examples/src/Views/Images.elm
+++ examples/src/Views/Images.elm
@@ -10,13 +10,13 @@ view =
         [ h3 [] [ text "Image Examples" ]
         , viewImageBlock "An informative image"
             "img \"Elm Logo\" [ src \"/assets/elm-logo.png\" ]"
-            (img "Elm Logo" [ src "/assets/elm-logo.png" ])
+            (img "Elm Logo" [ src "examples/assets/elm-logo.png" ])
         , viewImageBlock "A decorative image"
             "decorativeImg [ src \"/assets/elm-logo.png\" ]"
-            (decorativeImg [ src "/assets/elm-logo.png" ])
+            (decorativeImg [ src "examples/assets/elm-logo.png" ])
         , viewImageBlock "A functional image"
             "a [ href \"http://package.elm-lang.org/\" ] [ decorativeImg [ src \"/assets/elm-logo.png\" ] ]"
-            (a [ href "http://package.elm-lang.org/" ] [ decorativeImg [ src "/assets/elm-logo.png" ] ])
+            (a [ href "http://package.elm-lang.org/" ] [ decorativeImg [ src "examples/assets/elm-logo.png" ] ])
         ]
 
 
