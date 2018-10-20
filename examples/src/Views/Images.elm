9b5cc0c5daa104eef09284ad004c0beab8bf75fe
Tessa Kelly <tessakkelly@gmail.com>
2018-10-20 15:41:36 -0700

    Add example uses of the images

--- examples/src/Views/Images.elm
+++ examples/src/Views/Images.elm
@@ -8,18 +8,22 @@ view : Html msg
 view =
     div []
         [ h3 [] [ text "Image Examples" ]
-        , viewImageBlock "An informative image" <|
-            img "Elm Logo" [ src "/assets/elm-logo.png" ]
-        , viewImageBlock "A decorative image" <|
-            decorativeImg [ src "/assets/elm-logo.png" ]
-        , viewImageBlock "A functional image" <|
-            a [ href "http://package.elm-lang.org/" ] [ decorativeImg [ src "/assets/elm-logo.png" ] ]
+        , viewImageBlock "An informative image"
+            "img \"Elm Logo\" [ src \"/assets/elm-logo.png\" ]"
+            (img "Elm Logo" [ src "/assets/elm-logo.png" ])
+        , viewImageBlock "A decorative image"
+            "decorativeImg [ src \"/assets/elm-logo.png\" ]"
+            (decorativeImg [ src "/assets/elm-logo.png" ])
+        , viewImageBlock "A functional image"
+            "a [ href \"http://package.elm-lang.org/\" ] [ decorativeImg [ src \"/assets/elm-logo.png\" ] ]"
+            (a [ href "http://package.elm-lang.org/" ] [ decorativeImg [ src "/assets/elm-logo.png" ] ])
         ]
 
 
-viewImageBlock : String -> Html msg -> Html msg
-viewImageBlock header content =
+viewImageBlock : String -> String -> Html msg -> Html msg
+viewImageBlock header howToCall content =
     div []
-        [ h4 [] [ text header ]
+        [ h4 [ style "margin-bottom" "4px" ] [ text header ]
+        , code [ style "display" "block", style "margin" "4px 0" ] [ text howToCall ]
         , content
         ]
