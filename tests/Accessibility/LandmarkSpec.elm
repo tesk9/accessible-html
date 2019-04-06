8c074cccd91041f12a6f84a87871c172cf38a39f
Tessa Kelly <tessakkelly@gmail.com>
2019-04-05 18:40:52 -0700

    Revert ":skull: remove tests, travis, and npm packages"
    This reverts commit 1d97c6dc88bc39a96e5ab13a1cf5732b6c21e23c.

---
+++ tests/Accessibility/LandmarkSpec.elm
@@ -0,0 +1,37 @@
+module Accessibility.LandmarkSpec exposing (spec)
+
+import Accessibility as Html
+import Accessibility.Landmark exposing (..)
+import Html.Attributes
+import Json.Encode
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector as Selector
+
+
+spec : Test
+spec =
+    describe "Accessibility.Landmark" <|
+        List.map (\( a, b ) -> addsRole a b)
+            [ ( application, "application" )
+            , ( banner, "banner" )
+            , ( complementary, "complementary" )
+            , ( contentInfo, "contentinfo" )
+            , ( form, "form" )
+            , ( main_, "main" )
+            , ( navigation, "navigation" )
+            , ( region, "region" )
+            , ( search, "search" )
+            ]
+
+
+addsRole : Html.Attribute Never -> String -> Test
+addsRole role_ expected =
+    test ("sets the role attribute: " ++ toString role_) <|
+        \() ->
+            Html.div [] [ Html.div [ role_ ] [] ]
+                |> Query.fromHtml
+                |> Query.has
+                    [ Selector.attribute <|
+                        Html.Attributes.property "role" (Json.Encode.string expected)
+                    ]
