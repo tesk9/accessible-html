dcc96a003cd7f0f359a0a42dbc35ae2eb0926da9
Tessa Kelly <tessakkelly@gmail.com>
2022-01-09 13:18:59 -0800

    Update Style

--- src/Accessibility/Style.elm
+++ src/Accessibility/Style.elm
@@ -3,6 +3,11 @@ module Accessibility.Style exposing (invisible)
 {-|
 
 
+## Focus rings
+
+Please be aware that hiding a user's focus can make your site completely unusable for them. Learn more about focus ring styling options in [Understanding Success Criterion 2.4.7: Focus Visible](https://www.w3.org/WAI/WCAG21/Understanding/focus-visible).
+
+
 ## Hiding content
 
 For more information on hiding/semi-hiding elements, please see [the a11y project.](http://a11yproject.com/posts/how-to-hide-content/)
